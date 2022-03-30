6de3ee402f86ac55356ccc8593c3918f568cc997
LAPTOP-U10RCJLI\а715 <petrovartem353@gmail.com>
2022-03-30 21:34:30 +0300

    DeleGate.Artem

---
+++ Lesson5/study_delegate/Arbaiten.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_delegate
+{
+    delegate void ArtemDelegate();
+    delegate void ArtemDelegateOne(string x);
+    delegate void ArtemPlus(int aa, int bb);
+    delegate void ArtemMinus(int cc, int dd);
+    delegate int TovarPrice(int q, int w);
+    
+
+    public class Arbaiten
+    {
+        public static void Hi()
+        {
+            Console.WriteLine("Hi world !");
+        }
+        public static void Hello()
+        {
+            Console.WriteLine("Hello world !!");
+        }
+        public static void Hey()
+        {
+            Console.WriteLine("Hey world !!!");
+        }
+
+        public static void HiName(string name)
+        {
+            Console.WriteLine($"Hey {name}!!!");
+        }
+
+        public static void Plus (int a, int b)
+        {
+            Console.WriteLine($"Сумма чисел: {a+b}");
+        }
+
+        public static void Minus (int c, int d)
+        {
+            Console.WriteLine($"Разница чисел: {c - d}");
+        }
+
+        public static int Price(int ves, int cze)
+        {
+            int result = 0;
+            result = ves * cze;
+            return result;
+        }
+    }
+}
---
+++ Lesson5/study_delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace study_delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Arbaiten.Hello();
+            //Arbaiten.Hi();
+            //Arbaiten.Hey();
+            ArtemDelegate artem;
+            ArtemDelegateOne artemOne;
+            ArtemPlus plus;
+            ArtemMinus minus;
+            TovarPrice price;
+
+            artem = Arbaiten.Hello;
+            artem += Arbaiten.Hi;
+            artem += Arbaiten.Hello;
+            artem += Arbaiten.Hi;
+
+            artemOne = Arbaiten.HiName;
+
+            artem();
+            Console.WriteLine("/////////////////");
+            artemOne("Artem");
+            Console.WriteLine("+++++++++++++++++++++++++++++++");
+
+            plus = Arbaiten.Plus;
+            plus(2, 2);
+            Console.WriteLine("------------------------------");
+
+            minus = Arbaiten.Minus;
+            minus(5, 3);
+            Console.WriteLine("******************");
+
+            int asd = Arbaiten.Price(3,72);
+            price = Arbaiten.Price;
+            int result = price(13,72);
+            Console.WriteLine($"Ваша цена:{result}");
+            
+           
+            
+        }
+    }
+}
